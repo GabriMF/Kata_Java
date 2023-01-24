8fc17b66cf597f32133c8b362c051198bfb4048d
GabriMF <108740750+GabriMF@users.noreply.github.com>
2023-01-24 09:22:09 +0100

    fixing tests

--- src/test/java/com/factoriaf5/kata/CharacterTest.java
+++ src/test/java/com/factoriaf5/kata/CharacterTest.java
@@ -5,16 +5,25 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 
 public class CharacterTest {
-    
     @Test
-    public void canHealTest(){
-        //given
-        Character character = new Character();
-        //when
-        character.currentHealth = 50;
-        //then
-        boolean canHealTest = character.healer();
-        assertEquals(true, canHealTest);
+    public void initialHealthTest(){
+    Character character  = new Character();
+    int healthTest = character.health;
+    assertEquals(1000, healthTest);
+    }
+
+    @Test
+    public void initialLevelTest(){
+    Character character  = new Character();
+    int levelTest = character.level;
+    assertEquals(1, levelTest);
+    }
+
+    @Test
+    public void isAliveTest(){
+    Character character  = new Character();
+    boolean aliveTest = character.isAlive;
+    assertEquals(true, aliveTest);
     }
 
     @Test
@@ -26,5 +35,27 @@ public class CharacterTest {
         //Then
         int result = character.damageMaker();
         assertEquals(1, result);
+    }
+
+    @Test
+    public void isDeadTest() {
+        //given
+        Character character = new Character();
+        //When
+        character.currentHealth = 0 ;
+        //Then
+        boolean isItAlive = character.isAlive;
+        assertEquals(false, isItAlive);
+    }
+
+    @Test
+    public void canHealTest(){
+        //given
+        Character character = new Character();
+        //when
+        character.currentHealth = 50;
+        //then
+        boolean canHealTest = character.canHeal;
+        assertEquals(true, canHealTest);
     }
 }
